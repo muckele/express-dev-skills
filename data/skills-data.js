f23223985e88015c6d28c72f2161137a26b0c928
Mathew <muckele7@gmail.com>
2024-01-10 20:26:03 -0500

    Initial Commit

---
+++ data/skills-data.js
@@ -0,0 +1,11 @@
+const skills = [
+    {language: 'JavaScript', level: 'beginner', _id:789456},
+    {language: 'CSS', level: 'beginner', _id:789455},
+    {language: 'HTML', level: 'beginner', _id:789446},
+    {language: 'Node', level: 'beginner', _id:789856},
+
+]
+
+export {
+    skills
+}
---
+++ routes/skills.js
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import * as skillsCtrl from '../controllers/skills.js'
+
+const router = Router()
+
+// GET localhost:3000/users
+router.get('/', skillsCtrl.index)
+router.post('/id/reviews')
+
+export { router }
